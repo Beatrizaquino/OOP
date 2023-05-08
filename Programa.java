5b3b050703bf22dae6c3924318a7096f545a144d
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-05-07 21:20:51 -0400

    erro não resolvido

---
+++ Programa.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+    
+    public static void main(String[] args){
+
+        try (// Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in)) {
+            double  xB, xC, yA, yB, yC;
+            double xA;
+
+            System.out.println("Enter the measures of triangle X: ");
+            xA = scanner.nextDouble();
+            xB = scanner.nextDouble();
+            xC = scanner.nextDouble();
+            System.out.println("Enter the measures of triangle Y");
+            yA = scanner.nextDouble();
+            yB = scanner.nextDouble();
+            yC = scanner.nextDouble();
+
+             double p = (xA + xB + xC) / 2.0;
+             double areaX = Math.sqrt(p  * (p - xA) * (p - xB) * (p - xC));
+
+             p = (yA + yB + yC) / 2.0;
+             double areaY = Math.sqrt(p  * (p - yA) * (p - yB) * (p - yC));
+
+             System.out.printf("Triangle X area: %.4f%n", areaX);
+             System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+             if ( areaX > areaY){
+                 System.out.println("Large area: X");
+             } else {
+                 System.out.println("Large area: Y");
+             }
+        }
+
+    }
+    
+}
